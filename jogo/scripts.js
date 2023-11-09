ae61f918e5c014c0064321b27bb6f1009941b4d4
João Bolaséll <joaobolasell@hotmail.com>
2023-11-09 19:56:51 +0000

    a

--- jogo/scripts.js
+++ jogo/scripts.js
@@ -1,10 +1,13 @@
+
+
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
-
+let fim = 0;
 function flipCard() {
+  //if (fim) return;
   if (lockBoard) return;
   if (this === firstCard) return;
 
@@ -28,6 +31,15 @@ function checkForMatch() {
 }
 
 function disableCards() {
+  
+  
+  fim++;
+  fim == 6 ? alert("Parabéns, você ganhou!") : null;
+  if (fim == 6) {
+    pause();
+  }
+
+
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
 
@@ -58,3 +70,70 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+function pausarJogo(){
+  alert("Clique em OK para Continuar o Jogo");
+  
+}
+
+
+
+
+
+"use strict";
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+
+let cron;
+
+document.form_main.start.onclick = () => start();
+document.form_main.pause.onclick = () => pause();
+document.form_main.reset.onclick = () => reset();
+
+function start() {
+  pause();
+  cron = setInterval(() => { timer(); }, 10);
+}
+
+function pause() {
+  clearInterval(cron);
+}
+
+function reset() {
+  hour = 0;
+  minute = 0;
+  second = 0;
+  millisecond = 0;
+  document.getElementById('hour').innerText = '00';
+  document.getElementById('minute').innerText = '00';
+  document.getElementById('second').innerText = '00';
+  document.getElementById('millisecond').innerText = '000';
+}
+
+function timer() {
+  if ((millisecond += 10) == 1000) {
+    millisecond = 0;
+    second++;
+  }
+  if (second == 60) {
+    second = 0;
+    minute++;
+  }
+  if (minute == 60) {
+    minute = 0;
+    hour++;
+  }
+  document.getElementById('hour').innerText = returnData(hour);
+  document.getElementById('minute').innerText = returnData(minute);
+  document.getElementById('second').innerText = returnData(second);
+  document.getElementById('millisecond').innerText = returnData(millisecond);
+}
+
+function returnData(input) {
+  return input >= 10 ? input : `0${input}`
+}
