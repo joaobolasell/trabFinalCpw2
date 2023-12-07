4f2e2124f6fe993bd27c6e879ba7a6e19f9a0b46
João Bolaséll <joaobolasell@hotmail.com>
2023-12-07 07:06:16 +0000

    m

--- jogo/scripts.js
+++ jogo/scripts.js
@@ -1,51 +1,58 @@
 
-
+// seleciona todos os elementos com a classe memory-card
 const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false;
+let hasFlippedCard = false; // Variável para verificar se carta foi virada
 let lockBoard = false;
 let firstCard, secondCard;
 let fim = 0;
+
+
 function flipCard() {
-  //if (fim) return;
-  if (lockBoard) return;
-  if (this === firstCard) return;
+  if (lockBoard) return; // Se o tabuleiro estiver bloqueado, não faz nada
+  if (this === firstCard) return; // Se a carta clicada for a mesma, não faz nada
 
-  this.classList.add('flip');
+  this.classList.add('flip'); // Adiciona a classe flip ao elemento clicado para virar a carta no css
 
-  if (!hasFlippedCard) {
+  //(!hasFlippedCard)==true
+  // se nenhuma carta foi virada(hasFlippedCard = false) seta hasFlippedCard para true e seta a primeira carta clicada para firstCard
+  if (!hasFlippedCard) {                      
     hasFlippedCard = true;
     firstCard = this;
-
     return;
   }
-
+  // se a primeira carta já foi virada(hasFlippedCard = true) seta a segunda carta clicada para secondCard e chama a função checkForMatch para verificar se as cartas são iguais
   secondCard = this;
   checkForMatch();
 }
 
 function checkForMatch() {
+  // foi adicionado um data-framework para cada carta no html, contendo o valor da carta para comparação
+  // Verifica se o dataset da primeira carta clicada é igual ao dataset da segunda carta clicada
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
+  // se for igual, chama a função disableCards, se não chama a função unflipCards
   isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
-  
-  
+  // incrementa a variável fim a cada carta combinada, quando chegar a 6, pausa cronomêtro e acaba o jogo
   fim++;
   fim == 6 ? alert("Parabéns, você ganhou!") : null;
   if (fim == 6) {
     pause();
   }
 
-
+  // remove o evento de click das cartas para que não possam ser clicadas novamente
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
+  // reseta as variáveis para que o jogo possa continuar
   resetBoard();
 }
 
+// função para desvirar as cartas, remove a classe flip do css e reseta as variáveis para que o jogo possa continuar
+// lockBoard = true para que o usuário não possa clicar em outras cartas enquanto as cartas estão sendo desviradas
+// setTimeout para que o usuário possa ver as cartas antes de serem desviradas
 function unflipCards() {
   lockBoard = true;
 
@@ -62,6 +69,9 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
+// função para embaralhar as cartas
+// funcao imediatamente invocada, quando carrega a pagina, embaralha as cartas
+// Math.random() gera um numero aleatorio entre 0 e 11 e coloca na propriedade order 
 (function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
@@ -69,6 +79,7 @@ function resetBoard() {
   });
 })();
 
+// adiciona o evento de click para cada carta
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 //----------------------------------  -------------------------------------------------
@@ -85,43 +96,41 @@ document.getElementById("nomeResultado").textContent = nome;
 
 
 
-
-
-
-function pausarJogo(){
-  
+// Função para pausar o jogo
+function pausarJogo(){  
   pause();
   alert("Jogo pausado, clique em OK para continuar");
   start();
   }
-  
-
 
 /* ------------------------------- Cronometro ------------------------------- */
 
-
 "use strict";
-
+//variáveis de controle do timer 
 let hour = 0;
 let minute = 0;
 let second = 0;
 let millisecond = 0;
-
 let cron;
 
-document.form_main.start.onclick = () => start();
+//associa as funções aos seus botões 
 document.form_main.pause.onclick = () => pause();
 document.form_main.reset.onclick = () => reset();
 
+//inicia o Cronometro 
 function start() {
   pause();
   cron = setInterval(() => { timer(); }, 10);
 }
 
+//pausa o Cronometro mas não limpa as variáveis
 function pause() {
   clearInterval(cron);
 }
 
+
+
+//zera o Cronometro e insere manualmente os valores no HTML
 function reset() {
   hour = 0;
   minute = 0;
@@ -133,6 +142,7 @@ function reset() {
   document.getElementById('millisecond').innerText = '000';
 }
 
+//faz a contagem do tempo e exibição 
 function timer() {
   if ((millisecond += 10) == 1000) {
     millisecond = 0;
@@ -144,7 +154,7 @@ function timer() {
   }
   if (minute == 60) {
     minute = 0;
-    hour++;
+    hour++; 
   }
   document.getElementById('hour').innerText = returnData(hour);
   document.getElementById('minute').innerText = returnData(minute);
@@ -152,6 +162,10 @@ function timer() {
   document.getElementById('millisecond').innerText = returnData(millisecond);
 }
 
+//se o digito for menor que 10 então concatena com um 0 na frente
 function returnData(input) {
   return input >= 10 ? input : `0${input}`
 }
+
+
+
